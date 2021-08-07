fa3ca2830d10e2930e81d50b47aa332c75d8c93f
Sergey Dmitrenko <dmmytrenko@gmail.com>
2021-08-07 11:19:07 +0300

    Update README selection sort

--- SortingAlgorithms/SelectionSort.cs
+++ SortingAlgorithms/SelectionSort.cs
@@ -6,8 +6,24 @@ namespace SortingAlgorithms
     {
         public static List<int> Sort(List<int> array)
         {
-            
-            
+            for (var i = 0; i < array.Count; i++)
+            {
+                var min = i;
+                for(var j = i + 1; j < array.Count; j++) { 
+                    if(array[min] > array[j])
+                    {
+                        min = j;
+                    }
+                }
+
+                if(min != i)
+                {
+                    var lowerValue = array[min];
+                    array[min] = array[i];
+                    array[i] = lowerValue;
+                }
+            }
+            return array;
         }
     }
 }
